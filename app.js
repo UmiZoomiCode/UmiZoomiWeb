2285d6f9a2536f798e50725e407850972e42091a
Reese Bingham <reese.bingham@aggiemail.usu.edu>
2016-10-07 20:42:59 -0600

    Basic socket support.

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-(function(){
+var buttonHandler = (function(){
 	var socket = io('http://localhost:5000');
 	window.mySocket = socket;
 	socket.on('changedProfile', function(data){
 		console.log(data);
 	});
-	socket.on('connect', function(){
-		socket.emit('changeProfile', {Hello: "World"});
+	
+	socket.on('savedProfile', function(data){
+		console.log(data);
 	});
+	
+	socket.on('settings', function(data){
+		console.log(data);
+	});
+	
+	return function(type){
+		switch (type) {
+			case 'get':
+				socket.emit('getProfile');
+				break;
+			case 'change':
+				socket.emit('changeProfile');
+				break;
+			case 'save':
+				socket.emit('saveProfile');
+				break;
+			default:
+				break;
+		}
+	}
 })();
